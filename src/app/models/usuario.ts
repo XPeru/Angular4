72fc233c2d7567b8fa41bb4ef50f0f15c7ce594e
Oscar Garay <ojcgv21@gmail.com>
2018-07-23 01:54:44 +0200

    bugfix

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,11 +1,11 @@
 export interface Usuario {
-    id_usuario: number,
-    nombre: string,
-    apellidos: string,
-    imagen: string,
-    email: string,
-    create_time: Date,
-    update_time: Date,
-    is_active: Boolean,
-    tipo: string
+    id_usuario: number;
+    nombre: string;
+    apellidos: string;
+    imagen: string;
+    email: string;
+    create_time: Date;
+    update_time: Date;
+    is_active: Boolean;
+    tipo: string;
 }
